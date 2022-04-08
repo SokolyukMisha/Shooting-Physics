79fb0983284b7cb4ab542697210ee147864743a0
Misha <sokolyukmisha@gmail.com>
2022-04-08 12:35:50 +0300

    Refactroring
    Deleted useless part of the code

--- Assets/Scripts/Explosions.cs
+++ Assets/Scripts/Explosions.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class Explosions : MonoBehaviour
 {
-    GrenadeShoot grenade;
     [SerializeField] GameObject hitEffect;
 
-    void Start() {   
-        grenade = GetComponent<GrenadeShoot>();
-    }
     private void OnCollisionEnter(Collision collision)
-    {
-        
+    {        
         GameObject impact = Instantiate(hitEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
-        Destroy(impact, 15f);
+        Destroy(impact, 2f);
     }
 }
